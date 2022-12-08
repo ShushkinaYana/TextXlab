f407865cf6ad40371b9ba256de5c3f6ebe5a0c2e
Nuntius Nivis <y.shushkina@xlab-game.com>
2022-12-08 20:13:00 +0300

    08.12.200

--- Assets/Scripts/ToolChange.cs
+++ Assets/Scripts/ToolChange.cs
@@ -2,32 +2,13 @@ using UnityEngine;
 
 public class ToolChange : MonoBehaviour
 {
-    private int t_currentWeapon=0;
-    [SerializeField] private Transform[] t_weapons;
+    [SerializeField] Farmer[] m_farmer;
 
-    public void Update()
+   public void Action()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        foreach (var farmer in m_farmer)
         {
-            if (t_currentWeapon > t_weapons.Length)
-            {
-                t_currentWeapon = 0;
-            }
-            changeWeapon(t_currentWeapon);
-
-            t_currentWeapon++;
-        }
-        
-    }
-
-    public void changeWeapon(int num)
-    {
-        for (int i = 0; i < t_weapons.Length; i++)
-        {
-            if (i == num)
-                t_weapons[i].gameObject.SetActive(true);
-            else
-                t_weapons[i].gameObject.SetActive(false);
+            farmer.ChangeTool();
         }
     }
 }
